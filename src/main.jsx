e22fcbd76b48f7e9c4b4e9e422557ddae7e35730
devinammar <devinammar88@gmail.com>
2026-01-01 12:33:22 +0700

    Add recact router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home';
 import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <div>
+        <h1>tes</h1>
+        </div>
+    );
+};
