c7162527a811fc03be7582d1210ff4a96718158e
devinammar <devinammar88@gmail.com>
2026-01-05 22:22:06 +0700

    Add area2 component & variables on root

---
+++ src/components/home/area2.jsx
@@ -0,0 +1,11 @@
+import "./area2.css"
+
+export const Area2 = () => {
+    return (
+        <>
+        <div id="containerarea2">
+            <h1>tes</h1>
+        </div>
+        </>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import { Area1 } from '../components/home/area1'
+import { Area2 } from '../components/home/area2'
 
 export const Home = () => {
     return (
         <div className='grid'>
         <Area1 />
+        <Area2 />
         <h1>tes</h1>
         </div>
     );
