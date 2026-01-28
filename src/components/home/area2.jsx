37da19942a23107e35d844af467933c698564119
devinammar <devinammar88@gmail.com>
2026-01-28 18:01:04 +0700

    Area1 (Done) & Textarea2 component (Progress)

--- src/components/home/area2.jsx
+++ src/components/home/area2.jsx
@@ -6,7 +6,7 @@ export const Area2 = () => {
   return (
     <>
       <div id="containerarea2">
-        <Textarea2 h2area2="tes"/>
+        <Textarea2 h1area2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " parea2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam aliqua. Ut enim ad minim veniam ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam aliqua. Ut enim ad minim veniam ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam aliqua."/>
       </div>
     </>
   );
--- src/components/textarea2.jsx
+++ src/components/textarea2.jsx
@@ -1,12 +1,14 @@
 import "./textarea2.css";
 import { Link } from "react-router-dom";
 
-export const Textarea2 = ({h2area2, parea2}) => {
+export const Textarea2 = ({h1area2, parea2}) => {
   return (
     <div id="textarea2container">
-      <h2 id="h2area2">{h2area2}</h2>
+      <h1 id="h1area2">{h1area2}</h1>
       <p id="parea2">{parea2}</p>
-      <Link to="about">Learn About</Link>
+      <button id="buttonarea2">
+      <Link id="textbuttonarea2" to="about">Learn About</Link>
+      </button>
     </div>
   );
 };
