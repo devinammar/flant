2575bbaceb11d373309fde99bc70b197b6021ed5
devinammar <devinammar88@gmail.com>
2026-01-29 22:04:00 +0700

    Footer component (Done - remaining to upload the logo)

--- src/components/widebox.jsx
+++ src/components/widebox.jsx
@@ -3,6 +3,9 @@ import './widebox.css'
 export const Widebox = () => {
     return (
         <div className="containerwidebox">
+            <div id="block1widebox">
+                <p>tes</p>
+            </div>
         </div>
     )
 }
