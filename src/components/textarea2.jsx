db3054915c38d348e652fe7416e2e15db98711d7
devinammar <devinammar88@gmail.com>
2026-01-29 14:12:54 +0700

    Adjust line-height on footer component, textarea component & main.jsx

--- src/components/textarea2.jsx
+++ src/components/textarea2.jsx
@@ -5,7 +5,7 @@ export const Textarea2 = ({h1area2, parea2}) => {
   return (
     <div id="textarea2container">
       <h1 className="h1">{h1area2}</h1>
-      <p id="parea2">{parea2}</p>
+      <p className="p">{parea2}</p>
       <button id="buttonarea2">
       <Link id="textbuttonarea2" to="about">Learn More</Link>
       </button>
