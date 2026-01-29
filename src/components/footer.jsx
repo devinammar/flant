67a467fbe7b616ca9dcb29ffac38d356d9f44de6
devinammar <devinammar88@gmail.com>
2026-01-29 14:00:57 +0700

    Footer component (Progress - fix elements position)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,28 +2,62 @@ import "./footer.css";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
-    return (
-        <div id="footer">
-            <div id="logoarea">
-                <p>tes</p>
-            </div>
-            <div>
-            <h2>tes</h2>
-            <ul id="about">
-            <li className="listabout">
-                <Link to="/">Company</Link>
-            </li>
-            <li className="listabout">
-                <Link to="/">Team</Link>
-            </li>
-            <li className="listabout">
-                <Link to="/">Help & Support</Link>
-            </li>
-            <li className="listabout">
-                <Link to="/">License</Link>
-            </li>
+  return (
+    <div id="footer">
+      <div id="logoarea">
+        <p>tes</p>
+      </div>
+      <div className="listarea1">
+        <h2 className="h2">About</h2>
+        <ul id="about">
+          <li className="listabout">
+            <Link to="/">Company</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Team</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Help & Support</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">License</Link>
+          </li>
         </ul>
-        </div>
-        </div>
-    )
-}
+      </div>
+      <div className="listarea2">
+        <h2 className="h2">Resource</h2>
+        <ul id="about">
+          <li className="listabout">
+            <Link to="/">Company</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Team</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Help & Support</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">License</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="listarea3">
+        <h2 className="h2">Contact</h2>
+        <ul id="about">
+          <li className="listabout">
+            <Link to="/">Company</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Team</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">Help & Support</Link>
+          </li>
+          <li className="listabout">
+            <Link to="/">License</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/components/textarea2.jsx
+++ src/components/textarea2.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Textarea2 = ({h1area2, parea2}) => {
   return (
     <div id="textarea2container">
-      <h2 id="h1area2">{h1area2}</h2>
+      <h1 className="h1">{h1area2}</h1>
       <p id="parea2">{parea2}</p>
       <button id="buttonarea2">
       <Link id="textbuttonarea2" to="about">Learn More</Link>
