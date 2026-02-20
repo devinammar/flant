e839a05d6f0938565a039f9cb38bc7de1062ede9
devinammar <devinammar88@gmail.com>
2026-02-20 16:53:53 +0700

    Card component (Done)

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,13 +3,13 @@ import "./card.css";
 export const Card = () => {
   return (
     <div id="containercard">
-        <h1 id="h1card">With 2 main features</h1>
+        <h1 id="h1card">With 2 Main Features</h1>
     <div id="card1">
-        <h2 className="h2card">tes</h2>
+        <h2 className="h2card">Search</h2>
         <p className="pcard">tes</p>
     </div>
     <div id="card2">
-        <h2 className="h2card">tes</h2>
+        <h2 className="h2card">Search by Image</h2>
         <p className="pcard">tes</p>
     </div>
     </div>
