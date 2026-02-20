33847d5acde6b9a3dcf103f339f1cf513cbd4165
devinammar <devinammar88@gmail.com>
2026-02-20 17:48:46 +0700

    Adjusted element

--- src/components/widebox1.jsx
+++ src/components/widebox1.jsx
@@ -7,6 +7,7 @@ export const Widebox1 = ({ background }) => {
             <div id="block1widebox1">
                 <p>tes</p>
             </div>
+            <h1 id="h1widebox">Lorem ipsum dolor sit amet, consectetur adipiscing elit adipiscing</h1>
             <Link to="/login" id="block2widebox1">
                 <p id="buttonlogin">Login</p>
             </Link>
