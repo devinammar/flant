a3fb176ed1a677e5d5b33631ef52bda2772a41b5
devinammar <devinammar88@gmail.com>
2026-02-20 22:23:09 +0700

    Added cursor:pointer; to button in textarea2 component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,11 +6,11 @@ export const Card = () => {
         <h1 id="h1card">With 2 Main Features</h1>
     <div id="card1">
         <h2 className="h2card">Search</h2>
-        <p className="pcard">tes</p>
+        <p className="pcard">Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem lorem</p>
     </div>
     <div id="card2">
         <h2 className="h2card">Search by Image</h2>
-        <p className="pcard">tes</p>
+        <p className="pcard">Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem lorem</p>
     </div>
     </div>
   );
