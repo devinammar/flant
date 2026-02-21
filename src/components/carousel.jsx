db5842bde0ec37af56731b25517447f7385f8eac
devinammar <devinammar88@gmail.com>
2026-02-21 15:50:28 +0700

    Carousel component (Progress)

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,10 +1,30 @@
-export const Carousel = () => {
-    return(
-        <div id="carouselcontainer">
-            <h1 className="h1">Tes</h1>
-            <div id="carousel">
+import "./carousel.css";
 
-            </div>
-        </div>
-    )
-}
+const image = [
+  { id: 1, image: "/pexels-arrn-35442963.jpg" },
+  { id: 2, image: "/public/pexels-sonny-17133329.jpg" },
+  { id: 3, image: "/pexels-arrn-35442963.jpg" },
+  { id: 4, image: "/public/pexels-sonny-17133329.jpg" },
+  { id: 5, image: "/pexels-arrn-35442963.jpg" },
+];
+
+export const Carousel = () => {
+  return (
+    <div id="carouselcontainer">
+      <h1 className="h1">Tes</h1>
+      <div id="carousel">
+        {image.map((carousel) => (
+          <div key={carousel.id}>
+            <img
+              className={
+                carousel.id % 2 !== 0 ? "carouselimage1" : "carouselimage2"
+              }
+              src={carousel.image}
+              alt={`carousel-image-${carousel.id}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
