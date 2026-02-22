49c60e5d2c1a830d8eefb1304e822c01cd1d232c
devinammar <devinammar88@gmail.com>
2026-02-22 16:09:22 +0700

    Carousel component (Progress)

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -6,6 +6,11 @@ const image = [
   { id: 3, image: "/pexels-arrn-35442963.jpg" },
   { id: 4, image: "/public/pexels-sonny-17133329.jpg" },
   { id: 5, image: "/pexels-arrn-35442963.jpg" },
+  { id: 6, image: "/public/pexels-sonny-17133329.jpg" },
+  { id: 7, image: "/pexels-arrn-35442963.jpg" },
+  { id: 8, image: "/public/pexels-sonny-17133329.jpg" },
+  { id: 9, image: "/pexels-arrn-35442963.jpg" },
+  { id: 10, image: "/public/pexels-sonny-17133329.jpg" },
 ];
 
 export const Carousel = () => {
