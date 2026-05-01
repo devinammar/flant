069cbeb26e0ad8e7836b3a3bc089abb047596a48
Devin <devinammar88@gmail.com>
2026-05-01 13:27:29 +0000

    Add image (Done)

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,16 +1,16 @@
 import "./carousel.css";
 
 const image = [
-  { id: 1, image: "/pexels-arrn-35442963.jpg" },
-  { id: 2, image: "/public/pexels-sonny-17133329.jpg" },
-  { id: 3, image: "/pexels-arrn-35442963.jpg" },
-  { id: 4, image: "/public/pexels-sonny-17133329.jpg" },
-  { id: 5, image: "/pexels-arrn-35442963.jpg" },
-  { id: 6, image: "/public/pexels-sonny-17133329.jpg" },
-  { id: 7, image: "/pexels-arrn-35442963.jpg" },
-  { id: 8, image: "/public/pexels-sonny-17133329.jpg" },
-  { id: 9, image: "/pexels-arrn-35442963.jpg" },
-  { id: 10, image: "/public/pexels-sonny-17133329.jpg" },
+  { id: 1, image: "public/pexels-mithun-gahir-photography-266254368-13152523.jpg" },
+  { id: 2, image: "public/pexels-alexeydemidov-9314038.jpg" },
+  { id: 3, image: "public/georgia-de-lotz-vB-wJhosZpc-unsplash.jpg" },
+  { id: 4, image: "public/pexels-niranjan-t-g-56677497-7906305.jpg" },
+  { id: 5, image: "public/pexels-bianca-kida-2147898692-30003006.jpg" },
+  { id: 6, image: "public/pexels-alexeydemidov-12097650.jpg" },
+  { id: 7, image: "public/pexels-irfansimsar-32297052.jpg" },
+  { id: 8, image: "public/pexels-hartonosbg-34102675.jpg" },
+  { id: 9, image: "public/pexels-ronald-gavassa-2148721101-30245038.jpg" },
+  { id: 10, image: "public/pexels-laziccvanja-15510739.jpg" },
 ];
 
 const repeatedImages = [...image, ...image, ...image];
