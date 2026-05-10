79efe23deb66407087c9fc75cafb2faf2780302f
Devin <devinammar88@gmail.com>
2026-05-10 09:03:53 +0000

    Sidebar component (Progress)

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,22 +1,63 @@
+// ini hasil debug
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./sidebar.css";
 
 export const Sidebar = () => {
-    return(
-        <div id="sidebar">
-            <ul id="containersidebar">
-            <li className="listsidebar">
-                <Link to="/">Home</Link>
-            </li>
-            <li className="listsidebar">
-                <Link to="/search">Search</Link>
-            </li>
-            <li className="listsidebar">
-                <Link to="/searchbyimage">Search by Image</Link>
-            </li>
-            <li className="listsidebar">
-                <Link to="/about">About</Link>
-            </li>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button id="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+
+      {isOpen && (
+        <div id="overlay" onClick={() => setIsOpen(false)} />
+      )}
+
+      <div id="sidebar" className={isOpen ? "open" : ""}>
+        <ul id="containersidebar">
+          <li className="listsidebar">
+            <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+          </li>
+          <li className="listsidebar">
+            <Link to="/search" onClick={() => setIsOpen(false)}>Search</Link>
+          </li>
+          <li className="listsidebar">
+            <Link to="/searchbyimage" onClick={() => setIsOpen(false)}>Search by Image</Link>
+          </li>
+          <li className="listsidebar">
+            <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+          </li>
         </ul>
-        </div>
-    )
-}
+      </div>
+    </>
+  );
+};
+
+// ini bikinan sendiri (default)
+
+// import "./sidebar.css";
+
+// export const Sidebar = () => {
+//     return(
+//         <div id="sidebar">
+//             <ul id="containersidebar">
+//             <li className="listsidebar">
+//                 <Link to="/">Home</Link>
+//             </li>
+//             <li className="listsidebar">
+//                 <Link to="/search">Search</Link>
+//             </li>
+//             <li className="listsidebar">
+//                 <Link to="/searchbyimage">Search by Image</Link>
+//             </li>
+//             <li className="listsidebar">
+//                 <Link to="/about">About</Link>
+//             </li>
+//         </ul>
+//         </div>
+//     )
+// }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import { Area2 } from '../components/home/area2'
 import { Area3 } from '../components/home/area3'
 import { Area4 } from '../components/home/area4'
 import { Area5 } from '../components/home/area5'
+import { Sidebar } from '../components/sidebar'
 
 export const Home = () => {
     return (
@@ -12,6 +13,7 @@ export const Home = () => {
         <Area3 />
         <Area4 />
         <Area5 />
+        <Sidebar />
         </div>
     );
 };
