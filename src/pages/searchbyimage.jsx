93fca935b9cd8159a4e7964bf1c097c044a46495
Devin <devinammar88@gmail.com>
2026-05-11 04:17:45 +0000

    Widebox1 component on search by image page (progress)

--- src/pages/searchbyimage.jsx
+++ src/pages/searchbyimage.jsx
@@ -4,7 +4,7 @@ import { Sidebar } from '../components/sidebar'
 
 export const Searchbyimage = () => {
     return (
-        <div className='grid'>
+        <div className='gridsearchbyimage'>
         <Topbar />
         <Widebox1 background="color2" search="off" appearance="on"/>
         <Sidebar />
